b3de5638a6b0499ae5d65e96f9b0d32fe19181b9
LY VICHITH p1706375 <vichith.ly@etu.univ-lyon1.fr>
2020-05-29 13:39:05 +0200

    import github xavbox

---
+++ platforms/android/app/src/main/assets/www/js/app.js
@@ -0,0 +1,226 @@
+window.addEventListener('load', function () {
+
+    document.getElementById("a").addEventListener('click', function () {
+        var a = document.getElementById("1");
+        a.currentTime = 0;
+        a.play();
+    });
+
+    document.getElementById("b").addEventListener('click', function () {
+        var b = document.getElementById("2");
+        b.currentTime = 0;
+        b.play();
+    });
+
+    document.getElementById("c").addEventListener('click', function () {
+        var c = document.getElementById("3");
+        c.currentTime = 0;
+        c.play();
+    });
+
+    document.getElementById("d").addEventListener('click', function () {
+        var d = document.getElementById("4");
+        d.currentTime = 0;
+        d.play();
+    });
+
+    document.getElementById("e").addEventListener('click', function () {
+        var e = document.getElementById("5");
+        e.currentTime = 0;
+        e.play();
+    });
+
+    document.getElementById("f").addEventListener('click', function () {
+        var f = document.getElementById("6");
+        f.currentTime = 0;
+        f.play();
+    });
+
+    document.getElementById("g").addEventListener('click', function () {
+        var g = document.getElementById("7");
+        g.currentTime = 0;
+        g.play();
+    });
+
+    document.getElementById("h").addEventListener('click', function () {
+        var h = document.getElementById("8");
+        h.currentTime = 0;
+        h.play();
+    });
+
+    document.getElementById("i").addEventListener('click', function () {
+        var i = document.getElementById("9");
+        i.currentTime = 0;
+        i.play();
+    });
+
+    document.getElementById("j").addEventListener('click', function () {
+        var j = document.getElementById("10");
+        j.currentTime = 0;
+        j.play();
+    });
+
+    document.getElementById("k").addEventListener('click', function () {
+        var k = document.getElementById("11");
+        k.currentTime = 0;
+        k.play();
+    });
+
+    document.getElementById("l").addEventListener('click', function () {
+        var l = document.getElementById("12");
+        l.currentTime = 0;
+        l.play();
+    });
+    
+    document.getElementById("m").addEventListener('click', function () {
+        var m = document.getElementById("13");
+        m.currentTime = 0;
+        m.play();
+    });
+
+    document.getElementById("n").addEventListener('click', function () {
+        var n = document.getElementById("14");
+        n.currentTime = 0;
+        n.play();
+    });
+
+    document.getElementById("o").addEventListener('click', function () {
+        var o = document.getElementById("15");
+        o.currentTime = 0;
+        o.play();
+    });
+
+    document.getElementById("p").addEventListener('click', function () {
+        var p = document.getElementById("16");
+        p.currentTime = 0;
+        p.play();
+    });
+
+    document.getElementById("q").addEventListener('click', function () {
+        var q = document.getElementById("17");
+        q.currentTime = 0;
+        q.play();
+    });
+
+    document.getElementById("r").addEventListener('click', function () {
+        var r = document.getElementById("18");
+        r.currentTime = 0;
+        r.play();
+    });
+
+    document.getElementById("s").addEventListener('click', function () {
+        var s = document.getElementById("19");
+        s.currentTime = 0;
+        s.play();
+    });
+
+    document.getElementById("t").addEventListener('click', function () {
+        var t = document.getElementById("20");
+        t.currentTime = 0;
+        t.play();
+    });
+
+    document.getElementById("u").addEventListener('click', function () {
+        var u = document.getElementById("21");
+        u.currentTime = 0;
+        u.play();
+    });
+
+    document.getElementById("v").addEventListener('click', function () {
+        var v = document.getElementById("22");
+        v.currentTime = 0;
+        v.play();
+    });
+
+    document.getElementById("w").addEventListener('click', function () {
+        var w = document.getElementById("23");
+        w.currentTime = 0;
+        w.play();
+    });
+
+    document.getElementById("x").addEventListener('click', function () {
+        var x = document.getElementById("24");
+        x.currentTime = 0;
+        x.play();
+    });
+
+    document.getElementById("y").addEventListener('click', function () {
+        var y = document.getElementById("25");
+        y.currentTime = 0;
+        y.play();
+    });
+
+    document.getElementById("z").addEventListener('click', function () {
+        var z = document.getElementById("26");
+        z.currentTime = 0;
+        z.play();
+    });
+
+    document.getElementById("aa").addEventListener('click', function () {
+        var aa = document.getElementById("27");
+        aa.currentTime = 0;
+        aa.play();
+    });
+
+    document.getElementById("ab").addEventListener('click', function () {
+        var ab = document.getElementById("28");
+        ab.currentTime = 0;
+        ab.play();
+    });
+
+    document.getElementById("ac").addEventListener('click', function () {
+        var ac = document.getElementById("29");
+        ac.currentTime = 0;
+        ac.play();
+    });
+
+    document.getElementById("ad").addEventListener('click', function () {
+        var ad = document.getElementById("30");
+        ad.currentTime = 0;
+        ad.play();
+    });
+
+    document.getElementById("ae").addEventListener('click', function () {
+        var ae = document.getElementById("31");
+        ae.currentTime = 0;
+        ae.play();
+    });
+
+    document.getElementById("af").addEventListener('click', function () {
+        var af = document.getElementById("32");
+        af.currentTime = 0;
+        af.play();
+    });
+
+    document.getElementById("ag").addEventListener('click', function () {
+        var ag = document.getElementById("33");
+        ag.currentTime = 0;
+        ag.play();
+    });
+
+    document.getElementById("ah").addEventListener('click', function () {
+        var ah = document.getElementById("34");
+        ah.currentTime = 0;
+        ah.play();
+    });
+
+    document.getElementById("ai").addEventListener('click', function () {
+        var ai = document.getElementById("35");
+        ai.currentTime = 0;
+        ai.play();
+    });
+
+    document.getElementById("aj").addEventListener('click', function () {
+        var aj = document.getElementById("36");
+        aj.currentTime = 0;
+        aj.play();
+    });
+
+    document.getElementById("footer").addEventListener('click', function () {
+        var audio = document.getElementsByTagName("audio");
+        console.log(audio);
+        for (i = 0; i < audio.length; i++) {
+            audio[i].pause();
+        }
+    });
+});
